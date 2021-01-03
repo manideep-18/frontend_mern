1f957116b307fe2dcf87fbeedf8ed0747e37f396
manideep-18 <r8manideep@gmail.com>
2021-01-03 13:30:37 +0530

    Home,App components craeted with a simple text

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
 
+import Home from './core/Home'
+
 export default function Routes(){
     return(
         <Router>
