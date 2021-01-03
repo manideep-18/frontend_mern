c8b45fdedd7c7a97ebf80e4c0b7379484c9a78ba
manideep-18 <r8manideep@gmail.com>
2021-01-03 13:59:09 +0530

    backend url added in .env file

--- src/core/Home.js
+++ src/core/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import '../styles.css'
 
 export default function Home(){
+    console.log("API is",process.env.REACT_APP_BACKEND)
     return(
         <div>
             <h1 className="text-white">Hello front end</h1>
