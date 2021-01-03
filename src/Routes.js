56f11826f3c003e0075861f455ba36212759efab
manideep-18 <r8manideep@gmail.com>
2021-01-03 19:06:47 +0530

    signin page created

--- src/Routes.js
+++ src/Routes.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
 
 import Home from './core/Home'
+import Signin from './user/Signin'
 import Signup from './user/Signup'
 
 export default function Routes(){
@@ -10,6 +11,7 @@ export default function Routes(){
             <Switch>
                 <Route exact path='/' component={Home}/>
                 <Route path="/signup" component={Signup}/>
+                <Route path="/signin" component={Signin}/>
             </Switch>
         </Router>
     )
---
+++ src/user/Signin.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import Base from '../core/Base'
+
+export const Signin=()=>{
+    return(
+        <Base title="Sign in page" description="A page for user to sign in!">
+            <h1>Signin works</h1>
+        </Base>
+    )
+}
+
+export default Signin
