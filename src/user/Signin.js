f0bf4517f6ea77f909103ee0deb20ee19135177e
manideep-18 <r8manideep@gmail.com>
2021-01-03 19:24:17 +0530

    signup and signin form implemented

--- src/user/Signin.js
+++ src/user/Signin.js
@@ -3,9 +3,30 @@ import {Link} from 'react-router-dom'
 import Base from '../core/Base'
 
 export const Signin=()=>{
+
+    const SigninForm=()=>{
+        return(
+            <div className="row">
+                <div className="col-md-6 offset-sm-3 text-left">
+                    <form>
+                        <div className="form-group">
+                            <label className="text-light">Email</label>
+                            <input className="form-control" type="email"/>
+                        </div>
+                        <div className="form-group">
+                            <label className="text-light">Password</label>
+                            <input className="form-control" type="password"/>
+                        </div>
+                        <button className="btn btn-success btn-block">Submit</button>
+                    </form>
+                </div>
+            </div>
+        )
+    }
+
     return(
         <Base title="Sign in page" description="A page for user to sign in!">
-            <h1>Signin works</h1>
+            {SigninForm()}
         </Base>
     )
 }
