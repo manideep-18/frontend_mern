5ce82ba3611da40aed27217ff658821ea327a6ed
manideep-18 <r8manideep@gmail.com>
2021-01-03 19:05:03 +0530

    signup page created

--- src/Routes.js
+++ src/Routes.js
@@ -2,12 +2,14 @@ import React from 'react'
 import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
 
 import Home from './core/Home'
+import Signup from './user/Signup'
 
 export default function Routes(){
     return(
         <Router>
             <Switch>
                 <Route exact path='/' component={Home}/>
+                <Route path="/signup" component={Signup}/>
             </Switch>
         </Router>
     )
---
+++ src/user/Signup.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import Base from '../core/Base'
+
+export const Signup=()=>{
+    return(
+        <Base title="Sign up page" description="A page for user to sign up!">
+            <h1>Signup works</h1>
+        </Base>
+    )
+}
+
+export default Signup
