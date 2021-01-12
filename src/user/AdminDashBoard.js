d4b960d717f763bde353b7994284354be2ad5066
manideep-18 <r8manideep@gmail.com>
2021-01-12 16:54:18 +0530

    admin dashboard right hand side partially implemnted

--- src/user/AdminDashBoard.js
+++ src/user/AdminDashBoard.js
@@ -30,7 +30,22 @@ const AdminDashboard=()=>{
     }
 
     const adminRightSide=()=>{
-
+        return(
+            <div className="card mb-4">
+                <h4 className="card-header">Admin Information</h4>
+                <ul className="list-group">
+                    <li className="list-group-item">
+                        <span className="badge badge-success bg-success mr-2">Name:</span> {name}
+                    </li>
+                    <li className="list-group-item">
+                        <span className="badge badge-success bg-success mr-2">Email:</span> {email}
+                    </li>
+                    <li className="list-group-item">
+                        <span className="badge badge-danger bg-danger">Admin Area</span>
+                    </li>
+                </ul>
+            </div>
+        )
     }
 
     return (
