58a28bc9768fd670f26518d70120e1831988a03d
manideep-18 <r8manideep@gmail.com>
2021-01-12 15:26:48 +0530

    private,admin routes implemented

--- src/Routes.js
+++ src/Routes.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
+import { AdminRoute } from './auth/helper/AdminRoutes'
+import { PrivateRoute } from './auth/helper/PrivateRoutes'
 
 import Home from './core/Home'
+import AdminDashboard from './user/AdminDashBoard'
 import Signin from './user/Signin'
 import Signup from './user/Signup'
+import UserDashboard from './user/UserDashBoard'
 
 export default function Routes(){
     return(
@@ -12,6 +16,8 @@ export default function Routes(){
                 <Route exact path='/' component={Home}/>
                 <Route path="/signup" component={Signup}/>
                 <Route path="/signin" component={Signin}/>
+                <PrivateRoute path="/user/dashboard" exact component={UserDashboard}/>
+                <AdminRoute path="/admin/dashboard" exact component={AdminDashboard}/>
             </Switch>
         </Router>
     )
