fa6f0d566dcbde39412a10617887221c6ee4e2ce
manideep-18 <r8manideep@gmail.com>
2021-01-12 10:00:30 +0530

    signup text removed from signup form file

--- src/user/Signup.js
+++ src/user/Signup.js
@@ -30,7 +30,6 @@ export const Signup=()=>{
 
     return(
         <Base title="Sign up page" description="A page for user to sign up!">
-            <h1>Signup works</h1>
             {SignupForm()}
         </Base>
     )
