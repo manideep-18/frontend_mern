16f7ff424fd89d947ad381f4603defcc8b5440ba
manideep-18 <r8manideep@gmail.com>
2021-01-12 15:36:50 +0530

    signin redirection implemented

--- src/user/Signin.js
+++ src/user/Signin.js
@@ -63,10 +63,10 @@ export const Signin=()=>{
       const performRedirect = () =>{
           if(didRedirect){
               if(user && user.role===1){
-                  return (<p>redirect to admin</p>)
+                  return (<Redirect to="/admin/dashboard"/>)
               }
               else{
-                return (<p>redirect to user dashboard</p>)
+                return (<Redirect to="/user/dashboard"/>)
               }
           }
           if(isAuthenticated()){
