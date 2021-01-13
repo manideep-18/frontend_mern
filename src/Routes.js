a4c96200d98b0ece7b59512d95e62a8dac1517e7
manideep-18 <r8manideep@gmail.com>
2021-01-13 12:05:46 +0530

    Add product implemented partially

--- src/Routes.js
+++ src/Routes.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
 import AddCategory from './admin/AddCategory'
 import { ManageCategories } from './admin/ManageCategories'
+import { AddProduct } from './admin/AddProduct'
 import { AdminRoute } from './auth/helper/AdminRoutes'
 import { PrivateRoute } from './auth/helper/PrivateRoutes'
 
@@ -22,6 +23,7 @@ export default function Routes(){
                 <AdminRoute path="/admin/dashboard" exact component={AdminDashboard}/>
                 <AdminRoute path="/admin/create/category" exact component={AddCategory}/>
                 <AdminRoute path="/admin/categories" exact component={ManageCategories}/>
+                <AdminRoute path="/admin/create/product" exact component={AddProduct}/>
             </Switch>
         </Router>
     )
