abaef4db46053ce4ab975b2bbe374b0d1ae52912
manideep-18 <r8manideep@gmail.com>
2021-01-14 15:43:02 +0530

    addToCart functionality implemented

--- src/core/Card.js
+++ src/core/Card.js
@@ -1,16 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Redirect } from 'react-router-dom';
+import { addItemToCart } from './helper/cartHelper';
 import ImageHelper from './helper/ImageHelper';
 
 const Card = ({product,addtoCart = true,removefromCart=false}) => {
+
+    const [redirect,setRedirect]=useState(false)
 
     const cardTitle=product ? product.name:'A photo from pexels'
     const cardDescription=product ? product.description:'this photo looks great'
     const cardPrice=product ? product.price:'$ 5'
 
+    const addToCart=()=>{
+        addItemToCart(product,()=>setRedirect(true))
+    }
+
+    const getARedirect = (redirect)=>{
+        if(redirect){
+            return <Redirect to='/cart'/>
+        }
+    }
+
     const showAddtoCard =(addtoCart)=>{
         return (addtoCart &&  
         <button
-            onClick={() => {}}
+            onClick={addToCart}
             className="btn btn-block btn-outline-success mt-2 mb-2"
           >
             Add to Cart
@@ -32,6 +46,7 @@ const Card = ({product,addtoCart = true,removefromCart=false}) => {
       <div className="card text-white bg-dark border border-info ">
         <div className="card-header lead">{cardTitle}</div>
         <div className="card-body">
+            {getARedirect(redirect)}
          <ImageHelper product={product}/>
           <p className="lead bg-success font-weight-normal text-wrap">
             {cardDescription}
