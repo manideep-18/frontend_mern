2990e22eb7260b4563bca3a15110cce26eb8b7b2
manideep-18 <r8manideep@gmail.com>
2021-01-14 14:19:15 +0530

    getproducts implemented and used in home section

--- src/core/Card.js
+++ src/core/Card.js
@@ -3,6 +3,10 @@ import ImageHelper from './helper/ImageHelper';
 
 const Card = ({product,addtoCart = true,removefromCart=false}) => {
 
+    const cardTitle=product ? product.name:'A photo from pexels'
+    const cardDescription=product ? product.description:'this photo looks great'
+    const cardPrice=product ? product.price:'$ 5'
+
     const showAddtoCard =(addtoCart)=>{
         return (addtoCart &&  
         <button
@@ -26,13 +30,13 @@ const Card = ({product,addtoCart = true,removefromCart=false}) => {
 
     return (
       <div className="card text-white bg-dark border border-info ">
-        <div className="card-header lead">A photo from pexels</div>
+        <div className="card-header lead">{cardTitle}</div>
         <div className="card-body">
          <ImageHelper product={product}/>
           <p className="lead bg-success font-weight-normal text-wrap">
-            this photo looks great
+            {cardDescription}
           </p>
-          <p className="btn btn-success rounded  btn-sm px-4">$ 5</p>
+          <p className="btn btn-success rounded  btn-sm px-4">{cardPrice}</p>
           <div className="row">
             <div className="col-12">
              {showAddtoCard(addtoCart)}
--- src/core/Home.js
+++ src/core/Home.js
@@ -1,23 +1,43 @@
-import React from 'react'
-import { API } from '../backend'
+import React, { useEffect, useState } from 'react'
+
 import '../styles.css'
 import Base from './Base'
 import Card from './Card'
+import {getProducts} from './helper/coreapicalls'
 
 export default function Home(){
-    console.log("API is",API)
+    
+    const [products,setProducts] = useState([])
+    const [error,setError]=useState(false)
+
+    const loadAllProducts=()=>{
+        getProducts().then(data=>{
+            if(data.error){
+                setError(data.error)
+            }
+            else{
+                setProducts(data)
+            }
+        })
+    }
+
+    useEffect(()=>{
+        loadAllProducts()
+    },[])
+
     return(
         <Base title="Home Page" description="Welcome to the Tshirt Store">
             <div className="row text-center">
-                <div className="col-4">
-                    <Card/>
-                </div>
-                <div className="col-4">
-                <button className="btn btn-success">Test</button>
-                </div>
-                <div className="col-4">
-                <button className="btn btn-success">Test</button>
-                </div>
+               <h1 className="row text-center">All of tshirts</h1>
+               <div className="row">
+                {products.map((product,index)=>{
+                    return (
+                        <div key={index} className="col-4 mb-4">
+                            <Card product={product}/>
+                            </div>
+                    )
+                })}
+               </div>
             </div>
         </Base>
     )
