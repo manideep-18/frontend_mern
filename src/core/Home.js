f7e1e5db5f084295cdd2f27d9d8314dbf6fecb01
manideep-18 <r8manideep@gmail.com>
2021-01-14 13:23:47 +0530

    common card styles partially implemented

--- src/core/Home.js
+++ src/core/Home.js
@@ -2,14 +2,15 @@ import React from 'react'
 import { API } from '../backend'
 import '../styles.css'
 import Base from './Base'
+import Card from './Card'
 
 export default function Home(){
     console.log("API is",API)
     return(
         <Base title="Home Page" description="Welcome to the Tshirt Store">
-            <div className="row">
+            <div className="row text-center">
                 <div className="col-4">
-                    <button className="btn btn-success">Test</button>
+                    <Card/>
                 </div>
                 <div className="col-4">
                 <button className="btn btn-success">Test</button>
